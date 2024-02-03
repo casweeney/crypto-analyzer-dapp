4808bd9360af9982e5151d01008e8da9f60b3306
casweeney <casweeno2000@gmail.com>
2024-02-03 15:01:15 +0100

    Crypto intelligence tool completed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import "@covalenthq/goldrush-kit/styles.css";
-import {
-  GoldRushProvider,
-  NFTWalletTokenListView,
-  TokenBalancesListView,
-  TokenTransfersListView,
-  AddressActivityListView,
-} from "@covalenthq/goldrush-kit";
+import { Route, Switch } from 'react-router-dom';
+import Home from "./components/Home";
+import Activities from './components/Activities';
+import TokenTransfers from './components/TokenTransfers';
+import WalletAssets from './components/WalletAssets';
 
 function App() {
   return (
-    <div className="container mt-4 mb-5">
-      <GoldRushProvider apikey={process.env.REACT_APP_COVALENT_API_KEY} mode="dark" color="emerald">
-          {/* <TokenBalancesListView
-              chain_names={[
-                  "eth-mainnet",
-                  "matic-mainnet",
-                  "bsc-mainnet",
-                  "avalanche-mainnet",
-                  "optimism-mainnet",
-              ]}
-              hide_small_balances
-              address="0x65a465013862bCb3Ca8f8D7e06E86312A9d410d8"
-          /> */}
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
 
-          {/* <TokenTransfersListView
-              chain_name="eth-mainnet"
-              address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
-              contract_address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-          /> */}
+        <Route path="/activities/:address" exact>
+          <Activities />
+        </Route>
 
-          <AddressActivityListView address="0x65a465013862bCb3Ca8f8D7e06E86312A9d410d8" />
-      </GoldRushProvider>
+        <Route path="/wallet-assets/:address" exact>
+          <WalletAssets />
+        </Route>
+
+        <Route path="/token-transfers/:address/:token/" exact>
+          <TokenTransfers />
+        </Route>
+      </Switch>
     </div>
   );
 }
